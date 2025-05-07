f2191912b3fa406b775f1b1fe168173209492bf6
Laboratório FIAP <labsfiap@fiap.com.br>
2025-05-07 08:52:33 -0300

    feat: getProdutosByCategoriaId

--- src/main/java/br/com/fiap/ms_produto/controller/CategoriaController.java
+++ src/main/java/br/com/fiap/ms_produto/controller/CategoriaController.java
@@ -1,6 +1,7 @@
 package br.com.fiap.ms_produto.controller;
 
 import br.com.fiap.ms_produto.dto.CategoriaDTO;
+import br.com.fiap.ms_produto.dto.ProdutoResponseDTO;
 import br.com.fiap.ms_produto.service.CategoriaService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +18,12 @@ public class CategoriaController {
 
     @Autowired
     private CategoriaService service;
+
+    @GetMapping("/id/produtos")
+    public ResponseEntity<List<ProdutoResponseDTO>> findProdutoByCategoria(@PathVariable Long id) {
+        List<ProdutoResponseDTO> list = service.findProdutoByCategoria(id);
+        return ResponseEntity.ok(list);
+    }
 
     @GetMapping
     public ResponseEntity<List<CategoriaDTO>> findAll(){
